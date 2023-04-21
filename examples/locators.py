84f7d3de92f5a58eddc1cb763816fb60e1427de3
AutumnChill <huxiajie@outlook>
2023-04-21 20:25:31 +0800

    start waiting_strategy

---
+++ examples/locators.py
@@ -0,0 +1,9 @@
+"""统一管理locators"""
+
+from selenium.webdriver.common.by import By
+
+btn1_locator = (By.ID, "extensionId")
+btn2_locator = (By.ID, "extensionId")
+btn3_locator = (By.ID, "extensionId")
+btn4_locator = (By.ID, "extensionId")
+btn5_locator = (By.ID, "extensionId")
